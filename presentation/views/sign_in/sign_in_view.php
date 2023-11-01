b93e4642cd9797cf25fa5a24e773b13ad0d766f8
Manuel_Anton <manuelantoncisneros2023@gmail.com>
2023-11-01 00:20:13 -0500

    add: sing in presentation

---
+++ presentation/views/sign_in/sign_in_view.php
@@ -0,0 +1,49 @@
+<?php
+?>
+<!doctype html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="../../styles/sign-in-style.css">
+    <title>Document</title>
+</head>
+<body>
+<div class="wrapper">
+    <div class="container main">
+        <div class="row">
+            <div class="col-md-6 side-image">
+                <img src="../../resources/image/logo.png" alt="Logo chino cevicheria" class="img_logo">
+                <div class="text">
+                </div>
+            </div>
+
+            <div class="col-md-6 right">
+                <div class="input-box">
+                    <header>Inicio de Sesion</header>
+                    <div class="underline"></div>
+                    <form action="#">
+                        <div class="input-field">
+                            <input type="text" class="input" name="email" id="email" required autocomplete="off">
+                            <label for="email" class="">Email Address</label>
+                        </div>
+                        <div class="input-field">
+                            <input type="password" class="input" name="password" id="password" required autocomplete="off">
+                            <label for="password" class="">Password</label>
+                        </div>
+
+                        <div class="input-field">
+                            <button type="submit" class="submit">Sign in</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
