9d446f6737370b5bb18ab94694edc3d01c3382dd
Manuel_Anton <manuelantoncisneros2023@gmail.com>
2023-11-01 17:57:34 -0500

    add: home view

--- presentation/views/home/home_view.php
+++ presentation/views/home/home_view.php
@@ -53,76 +53,166 @@ if ($_GET) {
 <!doctype html>
 <html lang="es">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport"
-        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Inicio</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="../../styles/side-bar-style.css">
+    <title>Inicio</title>
+
 </head>
 <body>
-
-<h1>Datos del usuario:</h1>
-<p><?php echo $modelUser->__toString() ?></p>
-<h2>Si quieres acceder a una propiedad en específico lo haces así:</h2>
-<p><?php echo $modelUser->getName() ?></p>
-
-
-<h1>Categorías:</h1>
-<?php foreach ($categoriesDTO as $categoryDTO) { ?>
-  <p> <?php echo $categoryDTO->__toString() ?> </p>
-<?php } ?>
-
-<h1>En caso de las categorías, no tienes autocompletado pero igual funcionan, mira, el método getName de la línea 75
-  dice que no existe pero sí xd:</h1>
-<?php foreach ($categoriesDTO as $categoryDTO) { ?>
-  <p> <?php echo $categoryDTO->getName() ?> </p>
-<?php } ?>
-
-<h1>Producto más vendido:</h1>
-<?php echo $mostSellProductDTO->__toString() ?>
-<p>Cantidad de ventas: <?php echo $responseMostSellProduct['total_quantity'] ?></p>
-
-<h1>Pedidos:</h1>
-<h2>Historial de pedidos</h2>
-<p>Total de ventas de todoooooooo: S/ <?php echo($totalSales['total_sales']); ?></p>
-
-<h1>Acá te dejo un ejemplo en una tabla de la lista de órdenes, los 3 datos que están en el diseño: nombre del usuario,
-  id y precio total</h1>
-
-<table class="table">
-  <thead>
-  <tr>
-    <th scope="col">ID</th>
-    <th scope="col">Total</th>
-    <th scope="col">Usuario</th>
-  </tr>
-  </thead>
-  <tbody>
-  <?php foreach ($responseOrders as $order) { ?>
-    <tr>
-      <th scope="row"><?php echo $order['id'] ?></th>
-      <td>S/ <?php echo $order['total'] ?></td>
-      <td><?php echo $order['name'] ?></td>
-    </tr>
-  <?php } ?>
-  </tbody>
-</table>
-
-<h1>Acá te dejo otro ejemplo pero con especie de tarjetas, son de bootstrap</h1>
-<div class="row">
-    <?php foreach ($responseOrders as $order) { ?>
-      <div class="card p-0 m-2" style="width: 13rem;">
-        <img src="https://picsum.photos/400/400" class="card-img-top" alt="img">
-        <div class="card-body">
-          <h5 class="card-title"><?php echo $order['name'] ?></h5>
-          <h6 class="card-subtitle mb-2 text-muted"><?php echo $order['id'] ?></h6>
-          <p class="card-text"><?php echo $order['total'] ?></p>
+<div class=" position-fixed vw-100 vh-100 bg-light p-4">
+    <div class="vh-100 bg-default mx-auto d-flex side-bar">
+        <div class="side-menu bg-white vh-100">
+            <img class="mx-auto d-flex justify-content-center" src="../../resources/image/logo.png" alt="logo"/>
+            <section class="d-flex ps-8 gap-4 py-3 justify-content-start align-items-center btn-start">
+                <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 48 48"
+                     class="text-lg" height="1em" width="1em"
+                     xmlns="http://www.w3.org/2000/svg">
+                    <polygon fill="#E8EAF6" points="42,39 6,39 6,23 24,6 42,23"></polygon>
+                    <g fill="#C5CAE9">
+                        <polygon points="39,21 34,16 34,9 39,9"></polygon>
+                        <rect x="6" y="39" width="36" height="5"></rect>
+                    </g>
+                    <polygon fill="#B71C1C" points="24,4.3 4,22.9 6,25.1 24,8.4 42,25.1 44,22.9"></polygon>
+                    <rect x="18" y="28" fill="#D84315" width="12" height="16"></rect>
+                    <rect x="21" y="17" fill="#01579B" width="6" height="6"></rect>
+                    <path fill="#FF8A65"
+                          d="M27.5,35.5c-0.3,0-0.5,0.2-0.5,0.5v2c0,0.3,0.2,0.5,0.5,0.5S28,38.3,28,38v-2C28,35.7,27.8,35.5,27.5,35.5z"></path>
+                </svg>
+                <p>Inicio</p>
+            </section>
+            <section class="d-flex ps-8 gap-4 py-3 justify-content-start align-items-center btn-start">
+                <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 48 48"
+                     class="text-lg" height="1em" width="1em"
+                     xmlns="http://www.w3.org/2000/svg">
+                    <g fill="#3F51B5">
+                        <polygon points="17.8,18.1 10.4,25.4 6.2,21.3 4,23.5 10.4,29.9 20,20.3"></polygon>
+                        <polygon points="17.8,5.1 10.4,12.4 6.2,8.3 4,10.5 10.4,16.9 20,7.3"></polygon>
+                        <polygon points="17.8,31.1 10.4,38.4 6.2,34.3 4,36.5 10.4,42.9 20,33.3"></polygon>
+                    </g>
+                    <g fill="#90CAF9">
+                        <rect x="24" y="22" width="20" height="4"></rect>
+                        <rect x="24" y="9" width="20" height="4"></rect>
+                        <rect x="24" y="35" width="20" height="4"></rect>
+                    </g>
+                </svg>
+                <p>Pedidos</p>
+            </section>
+            <section class="d-flex ps-8 gap-4 py-3 justify-content-start align-items-center btn-start">
+                <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 48 48"
+                     class="text-lg" height="1em" width="1em"
+                     xmlns="http://www.w3.org/2000/svg">
+                    <g fill="#00BCD4">
+                        <rect x="19" y="22" width="10" height="20"></rect>
+                        <rect x="6" y="12" width="10" height="30"></rect>
+                        <rect x="32" y="6" width="10" height="36"></rect>
+                    </g>
+                </svg>
+                <p>Estadísticas</p>
+            </section>
+            <section class="d-flex ps-8 gap-4 py-3 justify-content-start align-items-center btn-start">
+                <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 48 48"
+                     class="text-lg" height="1em" width="1em"
+                     xmlns="http://www.w3.org/2000/svg">
+                    <path fill="#FFCCBC"
+                          d="M7,40V8c0-2.2,1.8-4,4-4h24c2.2,0,4,1.8,4,4v32c0,2.2-1.8,4-4,4H11C8.8,44,7,42.2,7,40z"></path>
+                    <g fill="#FF5722">
+                        <polygon points="42.7,24 32,33 32,15"></polygon>
+                        <rect x="14" y="21" width="23" height="6"></rect>
+                    </g>
+                </svg>
+                <p>Cerrar sesión</p>
+            </section>
         </div>
-      </div>
-    <?php } ?>
+        <div class="d-flex flex-column header-content">
+            <div class="d-flex justify-content-between align-items-center p-4 mt-4">
+                <h1 class="fs-4 header-text">Gestiona Tu Comercio</h1>
+                <div class="d-flex flex-column content-search">
+                    <input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Search">
+                </div>
+            </div>
+            <div class="m-4 p-4 bg-white rounded-2 shadow d-flex flex-column content-menu">
+                <h2>Menú</h2>
+                <p>Selecciona tu comida favorita</p>
+                <div class="row row-cols-4 row-cols-md-4 g-2">
+                    <div class="col">
+                        <div class="card">
+                            <img src="https://placehold.co/600x400" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <p class="card-text">Categoría 1</p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
+<!--<h1>Datos del usuario:</h1>-->
+<!--<p>--><?php //echo $modelUser->__toString() ?><!--</p>-->
+<!--<h2>Si quieres acceder a una propiedad en específico lo haces así:</h2>-->
+<!--<p>--><?php //echo $modelUser->getName() ?><!--</p>-->
+<!---->
+<!---->
+<!--<h1>Categorías:</h1>-->
+<?php //foreach ($categoriesDTO as $categoryDTO) { ?>
+<!--    <p> --><?php //echo $categoryDTO->__toString() ?><!-- </p>-->
+<?php //} ?>
+<!---->
+<!--<h1>En caso de las categorías, no tienes autocompletado pero igual funcionan, mira, el método getName de la línea 75-->
+<!--    dice que no existe pero sí xd:</h1>-->
+<?php //foreach ($categoriesDTO as $categoryDTO) { ?>
+<!--    <p> --><?php //echo $categoryDTO->getName() ?><!-- </p>-->
+<?php //} ?>
+<!---->
+<!--<h1>Producto más vendido:</h1>-->
+<?php //echo $mostSellProductDTO->__toString() ?>
+<!--<p>Cantidad de ventas: --><?php //echo $responseMostSellProduct['total_quantity'] ?><!--</p>-->
+<!---->
+<!--<h1>Pedidos:</h1>-->
+<!--<h2>Historial de pedidos</h2>-->
+<!--<p>Total de ventas de todoooooooo: S/ --><?php //echo($totalSales['total_sales']); ?><!--</p>-->
+<!---->
+<!--<h1>Acá te dejo un ejemplo en una tabla de la lista de órdenes, los 3 datos que están en el diseño: nombre del usuario,-->
+<!--    id y precio total</h1>-->
+<!---->
+<!--<table class="table">-->
+<!--    <thead>-->
+<!--    <tr>-->
+<!--        <th scope="col">ID</th>-->
+<!--        <th scope="col">Total</th>-->
+<!--        <th scope="col">Usuario</th>-->
+<!--    </tr>-->
+<!--    </thead>-->
+<!--    <tbody>-->
+<!--    --><?php //foreach ($responseOrders as $order) { ?>
+<!--        <tr>-->
+<!--            <th scope="row">--><?php //echo $order['id'] ?><!--</th>-->
+<!--            <td>S/ --><?php //echo $order['total'] ?><!--</td>-->
+<!--            <td>--><?php //echo $order['name'] ?><!--</td>-->
+<!--        </tr>-->
+<!--    --><?php //} ?>
+<!--    </tbody>-->
+<!--</table>-->
+<!---->
+<!--<h1>Acá te dejo otro ejemplo pero con especie de tarjetas, son de bootstrap</h1>-->
+<!--<div class="row">-->
+<!--    --><?php //foreach ($responseOrders as $order) { ?>
+<!--        <div class="card p-0 m-2" style="width: 13rem;">-->
+<!--            <img src="https://picsum.photos/400/400" class="card-img-top" alt="img">-->
+<!--            <div class="card-body">-->
+<!--                <h5 class="card-title">--><?php //echo $order['name'] ?><!--</h5>-->
+<!--                <h6 class="card-subtitle mb-2 text-muted">--><?php //echo $order['id'] ?><!--</h6>-->
+<!--                <p class="card-text">--><?php //echo $order['total'] ?><!--</p>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--    --><?php //} ?>
+<!--</div>-->
 
 </body>
 </html>
