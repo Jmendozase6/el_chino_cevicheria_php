161058ae09caac21faa71014ded3b206ae708b2f
Jhair Mendoza <Felinecode.dev@gmail.com>
2023-11-05 00:29:52 -0500

    update: script

--- script.sql
+++ script.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 01-11-2023 a las 16:01:53
+-- Tiempo de generación: 05-11-2023 a las 06:29:14
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.0.28
 
@@ -12,9 +12,9 @@ START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT = @@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS = @@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION = @@COLLATION_CONNECTION */;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
 --
@@ -27,13 +27,10 @@ SET time_zone = "+00:00";
 -- Estructura de tabla para la tabla `cart`
 --
 
-CREATE TABLE `cart`
-(
-    `id`       int(11) NOT NULL,
-    `order_id` int(11) DEFAULT NULL
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4
-  COLLATE = utf8mb4_general_ci;
+CREATE TABLE `cart` (
+                        `id` int(11) NOT NULL,
+                        `order_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -41,23 +38,23 @@ CREATE TABLE `cart`
 -- Estructura de tabla para la tabla `category`
 --
 
-CREATE TABLE `category`
-(
-    `id`         int(11)     NOT NULL,
-    `name`       varchar(20) NOT NULL,
-    `img`        varchar(1000)        DEFAULT NULL,
-    `created_at` timestamp   NOT NULL DEFAULT current_timestamp()
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4
-  COLLATE = utf8mb4_general_ci;
+CREATE TABLE `category` (
+                            `id` int(11) NOT NULL,
+                            `name` varchar(20) NOT NULL,
+                            `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+                            `img` varchar(100) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `category`
 --
 
-INSERT INTO `category` (`id`, `name`, `created_at`)
-VALUES (1, 'Bebidas', '2023-10-31 06:38:34'),
-       (2, 'Ceviches', '2023-10-31 06:38:51');
+INSERT INTO `category` (`id`, `name`, `created_at`, `img`) VALUES
+                                                               (1, 'Bebidas', '2023-10-31 06:38:34', 'https://res.cloudinary.com/dk3uxckba/image/upload/v1699158083/b7jrjpz6gjskyzchel0u.jpg'),
+                                                               (2, 'Ceviches', '2023-10-31 06:38:51', 'https://res.cloudinary.com/dk3uxckba/image/upload/v1699158083/sljv0o7lbjrumzg7eba1.jpg'),
+                                                               (3, 'Entradas', '2023-11-05 04:11:29', 'https://res.cloudinary.com/dk3uxckba/image/upload/v1699158083/a3idodddn2rvekrqwo3d.jpg'),
+                                                               (4, 'ssdsd', '2023-11-05 04:14:49', 'https://res.cloudinary.com/dk3uxckba/image/upload/v1699158083/odm2wduy1zp4oo8euti0.jpg'),
+                                                               (5, 'sdfsdf', '2023-11-05 04:14:49', 'https://res.cloudinary.com/dk3uxckba/image/upload/v1699158084/mftogjar4lbuswlsb9kz.jpg\n');
 
 -- --------------------------------------------------------
 
@@ -65,25 +62,22 @@ VALUES (1, 'Bebidas', '2023-10-31 06:38:34'),
 -- Estructura de tabla para la tabla `order`
 --
 
-CREATE TABLE `order`
-(
-    `id`         int(11)   NOT NULL,
-    `user_id`    int(11)            DEFAULT NULL,
-    `payment_id` int(11)            DEFAULT NULL,
-    `total`      decimal(10, 0)     DEFAULT NULL,
-    `created_at` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4
-  COLLATE = utf8mb4_general_ci;
+CREATE TABLE `order` (
+                         `id` int(11) NOT NULL,
+                         `user_id` int(11) DEFAULT NULL,
+                         `payment_id` int(11) DEFAULT NULL,
+                         `total` decimal(10,0) DEFAULT NULL,
+                         `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `order`
 --
 
-INSERT INTO `order` (`id`, `user_id`, `payment_id`, `total`, `created_at`)
-VALUES (1, 1, 1, 100, '2023-10-31 06:59:58'),
-       (2, 2, 2, 100, '2023-10-31 07:01:22'),
-       (3, 1, 3, 300, '2023-10-31 07:01:22');
+INSERT INTO `order` (`id`, `user_id`, `payment_id`, `total`, `created_at`) VALUES
+                                                                               (1, 1, 1, 100, '2023-10-31 06:59:58'),
+                                                                               (2, 2, 2, 100, '2023-10-31 07:01:22'),
+                                                                               (3, 1, 3, 300, '2023-10-31 07:01:22');
 
 -- --------------------------------------------------------
 
@@ -91,24 +85,21 @@ VALUES (1, 1, 1, 100, '2023-10-31 06:59:58'),
 -- Estructura de tabla para la tabla `order_product`
 --
 
-CREATE TABLE `order_product`
-(
-    `id`         int(11) NOT NULL,
-    `order_id`   int(11) DEFAULT NULL,
-    `product_id` int(11) DEFAULT NULL,
-    `quantity`   int(11) DEFAULT NULL
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4
-  COLLATE = utf8mb4_general_ci;
+CREATE TABLE `order_product` (
+                                 `id` int(11) NOT NULL,
+                                 `order_id` int(11) DEFAULT NULL,
+                                 `product_id` int(11) DEFAULT NULL,
+                                 `quantity` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `order_product`
 --
 
-INSERT INTO `order_product` (`id`, `order_id`, `product_id`, `quantity`)
-VALUES (1, 1, 1, 1),
-       (2, 1, 2, 1),
-       (3, 2, 2, 3);
+INSERT INTO `order_product` (`id`, `order_id`, `product_id`, `quantity`) VALUES
+                                                                             (1, 1, 1, 1),
+                                                                             (2, 1, 2, 1),
+                                                                             (3, 2, 2, 3);
 
 -- --------------------------------------------------------
 
@@ -116,23 +107,20 @@ VALUES (1, 1, 1, 1),
 -- Estructura de tabla para la tabla `payment_method`
 --
 
-CREATE TABLE `payment_method`
-(
-    `id`      int(11)      NOT NULL,
-    `name`    varchar(255) NOT NULL,
-    `receipt` text         NOT NULL
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4
-  COLLATE = utf8mb4_general_ci;
+CREATE TABLE `payment_method` (
+                                  `id` int(11) NOT NULL,
+                                  `name` varchar(255) NOT NULL,
+                                  `receipt` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `payment_method`
 --
 
-INSERT INTO `payment_method` (`id`, `name`, `receipt`)
-VALUES (1, 'Yape', 'yape-receipt.png'),
-       (2, 'Plin', 'plin-receipt.png'),
-       (3, 'Yape', 'yape-receipt.png');
+INSERT INTO `payment_method` (`id`, `name`, `receipt`) VALUES
+                                                           (1, 'Yape', 'yape-receipt.png'),
+                                                           (2, 'Plin', 'plin-receipt.png'),
+                                                           (3, 'Yape', 'yape-receipt.png');
 
 -- --------------------------------------------------------
 
@@ -140,30 +128,26 @@ VALUES (1, 'Yape', 'yape-receipt.png'),
 -- Estructura de tabla para la tabla `product`
 --
 
-CREATE TABLE `product`
-(
-    `id`          int(11)        NOT NULL,
-    `id_category` int(11)                 DEFAULT NULL,
-    `name`        varchar(30)    NOT NULL,
-    `description` varchar(255)            DEFAULT NULL,
-    `image`       varchar(255)            DEFAULT NULL,
-    `price`       decimal(10, 0) NOT NULL,
-    `active`      tinyint(1)              DEFAULT 1,
-    `created_at`  timestamp      NOT NULL DEFAULT current_timestamp(),
-    `stock`       decimal(10, 0)          DEFAULT NULL,
-    `discount`    decimal(10, 0)          DEFAULT NULL
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4
-  COLLATE = utf8mb4_general_ci;
+CREATE TABLE `product` (
+                           `id` int(11) NOT NULL,
+                           `id_category` int(11) DEFAULT NULL,
+                           `name` varchar(30) NOT NULL,
+                           `description` varchar(255) DEFAULT NULL,
+                           `image` varchar(255) DEFAULT NULL,
+                           `price` decimal(10,0) NOT NULL,
+                           `active` tinyint(1) DEFAULT 1,
+                           `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+                           `stock` decimal(10,0) DEFAULT NULL,
+                           `discount` decimal(10,0) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `product`
 --
 
-INSERT INTO `product` (`id`, `id_category`, `name`, `description`, `image`, `price`, `active`, `created_at`, `stock`,
-                       `discount`)
-VALUES (1, 1, 'Agua', NULL, 'agua.png', 2, 1, '2023-10-31 07:03:03', 10, 0),
-       (2, 2, 'Coca', NULL, 'ceviche.png', 10, 1, '2023-10-31 07:03:03', 10, 0);
+INSERT INTO `product` (`id`, `id_category`, `name`, `description`, `image`, `price`, `active`, `created_at`, `stock`, `discount`) VALUES
+                                                                                                                                      (1, 1, 'Agua', NULL, 'agua.png', 2, 1, '2023-10-31 07:03:03', 10, 0),
+                                                                                                                                      (2, 2, 'Coca', NULL, 'ceviche.png', 10, 1, '2023-10-31 07:03:03', 10, 0);
 
 -- --------------------------------------------------------
 
@@ -171,14 +155,11 @@ VALUES (1, 1, 'Agua', NULL, 'agua.png', 2, 1, '2023-10-31 07:03:03', 10, 0),
 -- Estructura de tabla para la tabla `review`
 --
 
-CREATE TABLE `review`
-(
-    `id`      int(11)      NOT NULL,
-    `user_id` int(11) DEFAULT NULL,
-    `review`  varchar(200) NOT NULL
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4
-  COLLATE = utf8mb4_general_ci;
+CREATE TABLE `review` (
+                          `id` int(11) NOT NULL,
+                          `user_id` int(11) DEFAULT NULL,
+                          `review` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -186,22 +167,19 @@ CREATE TABLE `review`
 -- Estructura de tabla para la tabla `role`
 --
 
-CREATE TABLE `role`
-(
-    `id`   int(11) NOT NULL,
-    `role` varchar(30) DEFAULT NULL
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4
-  COLLATE = utf8mb4_general_ci;
+CREATE TABLE `role` (
+                        `id` int(11) NOT NULL,
+                        `role` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `role`
 --
 
-INSERT INTO `role` (`id`, `role`)
-VALUES (1, 'admin'),
-       (2, 'collaborator'),
-       (3, 'client');
+INSERT INTO `role` (`id`, `role`) VALUES
+                                      (1, 'admin'),
+                                      (2, 'collaborator'),
+                                      (3, 'client');
 
 -- --------------------------------------------------------
 
@@ -209,13 +187,10 @@ VALUES (1, 'admin'),
 -- Estructura de tabla para la tabla `sale`
 --
 
-CREATE TABLE `sale`
-(
-    `id`       int(11) NOT NULL,
-    `order_id` int(11) NOT NULL
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4
-  COLLATE = utf8mb4_general_ci;
+CREATE TABLE `sale` (
+                        `id` int(11) NOT NULL,
+                        `order_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -223,16 +198,13 @@ CREATE TABLE `sale`
 -- Estructura de tabla para la tabla `schedule`
 --
 
-CREATE TABLE `schedule`
-(
-    `id`         int(11)     NOT NULL,
-    `day`        varchar(10) NOT NULL,
-    `open_time`  timestamp   NOT NULL DEFAULT current_timestamp(),
-    `close_time` timestamp   NOT NULL DEFAULT current_timestamp(),
-    `created_at` timestamp   NOT NULL DEFAULT current_timestamp()
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4
-  COLLATE = utf8mb4_general_ci;
+CREATE TABLE `schedule` (
+                            `id` int(11) NOT NULL,
+                            `day` varchar(10) NOT NULL,
+                            `open_time` timestamp NOT NULL DEFAULT current_timestamp(),
+                            `close_time` timestamp NOT NULL DEFAULT current_timestamp(),
+                            `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -240,30 +212,26 @@ CREATE TABLE `schedule`
 -- Estructura de tabla para la tabla `user`
 --
 
-CREATE TABLE `user`
-(
-    `id`         int(11)   NOT NULL,
-    `id_role`    int(11)            DEFAULT NULL,
-    `id_address` int(11)            DEFAULT NULL,
-    `name`       varchar(50)        DEFAULT NULL,
-    `last_name`  varchar(50)        DEFAULT NULL,
-    `email`      varchar(100)       DEFAULT NULL,
-    `password`   varchar(255)       DEFAULT NULL,
-    `active`     tinyint(1)         DEFAULT 1,
-    `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-    `img`        varchar(1000)      DEFAULT NULL
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4
-  COLLATE = utf8mb4_general_ci;
+CREATE TABLE `user` (
+                        `id` int(11) NOT NULL,
+                        `id_role` int(11) DEFAULT NULL,
+                        `id_address` int(11) DEFAULT NULL,
+                        `name` varchar(50) DEFAULT NULL,
+                        `last_name` varchar(50) DEFAULT NULL,
+                        `email` varchar(100) DEFAULT NULL,
+                        `password` varchar(255) DEFAULT NULL,
+                        `active` tinyint(1) DEFAULT 1,
+                        `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+                        `img` varchar(1000) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `user`
 --
 
-INSERT INTO `user` (`id`, `id_role`, `id_address`, `name`, `last_name`, `email`, `password`, `active`, `created_at`,
-                    `img`)
-VALUES (1, 1, NULL, 'Jhair', 'Mendoza', 'jhair@gmail.com', '12345', 1, '2023-10-31 06:10:45', NULL),
-       (2, 1, NULL, 'Liz', 'Sosa', 'liz@gmail.com', '12345', 1, '2023-10-31 07:00:28', NULL);
+INSERT INTO `user` (`id`, `id_role`, `id_address`, `name`, `last_name`, `email`, `password`, `active`, `created_at`, `img`) VALUES
+                                                                                                                                (1, 1, NULL, 'Jhair', 'Mendoza', 'jhair@gmail.com', '12345', 1, '2023-10-31 06:10:45', NULL),
+                                                                                                                                (2, 1, NULL, 'Liz', 'Sosa', 'liz@gmail.com', '12345', 1, '2023-10-31 07:00:28', NULL);
 
 -- --------------------------------------------------------
 
@@ -271,14 +239,11 @@ VALUES (1, 1, NULL, 'Jhair', 'Mendoza', 'jhair@gmail.com', '12345', 1, '2023-10-
 -- Estructura de tabla para la tabla `user_address`
 --
 
-CREATE TABLE `user_address`
-(
-    `id`      int(11)      NOT NULL,
-    `user_id` int(11)      NOT NULL,
-    `address` varchar(150) NOT NULL
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4
-  COLLATE = utf8mb4_general_ci;
+CREATE TABLE `user_address` (
+                                `id` int(11) NOT NULL,
+                                `user_id` int(11) NOT NULL,
+                                `address` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Índices para tablas volcadas
@@ -381,36 +346,31 @@ ALTER TABLE `cart`
 -- AUTO_INCREMENT de la tabla `category`
 --
 ALTER TABLE `category`
-    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,
-    AUTO_INCREMENT = 3;
+    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `order`
 --
 ALTER TABLE `order`
-    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,
-    AUTO_INCREMENT = 4;
+    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `order_product`
 --
 ALTER TABLE `order_product`
-    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,
-    AUTO_INCREMENT = 4;
+    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `payment_method`
 --
 ALTER TABLE `payment_method`
-    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,
-    AUTO_INCREMENT = 4;
+    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `product`
 --
 ALTER TABLE `product`
-    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,
-    AUTO_INCREMENT = 3;
+    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `review`
@@ -422,8 +382,7 @@ ALTER TABLE `review`
 -- AUTO_INCREMENT de la tabla `role`
 --
 ALTER TABLE `role`
-    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,
-    AUTO_INCREMENT = 4;
+    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `sale`
@@ -441,8 +400,7 @@ ALTER TABLE `schedule`
 -- AUTO_INCREMENT de la tabla `user`
 --
 ALTER TABLE `user`
-    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,
-    AUTO_INCREMENT = 3;
+    MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `user_address`
@@ -500,6 +458,6 @@ ALTER TABLE `user`
     ADD CONSTRAINT `user_ibfk_2` FOREIGN KEY (`id_address`) REFERENCES `user_address` (`id`);
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT = @OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS = @OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION = @OLD_COLLATION_CONNECTION */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
