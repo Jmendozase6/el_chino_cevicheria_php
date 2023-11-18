8c31492719fa9512e3e7a7c1b09f6861b7905207
Manuel_Anton <manuelantoncisneros2023@gmail.com>
2023-11-17 21:49:22 -0500

    fix: dashboard view

--- presentation/views/catalog_client/catalog_client.php
+++ presentation/views/catalog_client/catalog_client.php
@@ -1,2 +1,16 @@
 <?php
 
+require_once '../../../data_access_objects/ProductDAO.php';
+require_once '../../../data_access_objects/CartDAO.php';
+
+if (isset($_GET['productId']) && $_GET['productId'] != "") {
+
+  $productsDAO = new ProductDAO();
+  $cartDAO = new CartDAO();
+
+  $productId = $_GET['productId'];
+  $cartDAO->addProductToCart($productId, 1);
+
+  header("Location:javascript://history.go(-1)");
+}
+?>
